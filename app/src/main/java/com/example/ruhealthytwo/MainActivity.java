7d4affda8f68f955f7438bbc70f3ba87c344f440
Pranav Komarla <pranavkv03@gmail.com>
2024-03-23 19:14:55 -0400

    Finished Login and Sign Up User Authentication

--- app/src/main/java/com/example/ruhealthytwo/MainActivity.java
+++ app/src/main/java/com/example/ruhealthytwo/MainActivity.java
@@ -3,13 +3,21 @@ package com.example.ruhealthytwo;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 //public class MainActivity extends AppCompatActivity {
 //
@@ -54,6 +62,10 @@ public class MainActivity extends AppCompatActivity {
     private EditText password;
     private Button loginButton;
     private TextView signUpText;
+
+    private FirebaseAuth mAuth;
+
+    private static final String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -63,6 +75,7 @@ public class MainActivity extends AppCompatActivity {
         loginButton = findViewById(R.id.registerButton);
 
         signUpText = findViewById(R.id.signupText);
+        mAuth = FirebaseAuth.getInstance();
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -70,12 +83,15 @@ public class MainActivity extends AppCompatActivity {
                 String passwordtxt = password.getText().toString();
 
                 if(TextUtils.isEmpty(emailtxt) || TextUtils.isEmpty(passwordtxt)) {
-                    Toast.makeText(MainActivity.this, "Login Successful!", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this, "Empty Credentials", Toast.LENGTH_SHORT).show();
                 } else {
-                    Toast.makeText(MainActivity.this, "Login Failed!", Toast.LENGTH_SHORT).show();
+                    //Toast.makeText(MainActivity.this, "Login Failed!", Toast.LENGTH_SHORT).show();
+                    signInUser(emailtxt, passwordtxt);
                 }
             }
         });
+
+
 
         signUpText.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -86,5 +102,31 @@ public class MainActivity extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+    private void signInUser(String email, String password) {
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+                            Toast.makeText(MainActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                            FirebaseUser user = mAuth.getCurrentUser();
+
+                            //updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(MainActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                            //updateUI(null);
+                        }
+                    }
+                });
+    }
+
+    private void updateUI(FirebaseUser user) {
     }
 }
