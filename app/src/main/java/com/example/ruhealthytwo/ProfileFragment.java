bf9356edb9d9ba89a94f1036ae2e510527c897ad
pranav sudheer <pranav.sudheer19@gmail.com>
2024-03-24 05:40:56 -0400

    TabLayout Function

--- app/src/main/java/com/example/ruhealthytwo/ProfileFragment.java
+++ app/src/main/java/com/example/ruhealthytwo/ProfileFragment.java
@@ -1,4 +1,3 @@
-/*
 package com.example.ruhealthytwo;
 
 import android.os.Bundle;
@@ -12,26 +11,14 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import com.example.ruhealthytwo.R;
-
-
-public class DemoFragment extends Fragment {
-
+public class ProfileFragment extends Fragment {
 
     public static final String TITLE = "title";
-    public DemoFragment() {
-        // Required empty public constructor
-    }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_demo, container, false);
+        return inflater.inflate(R.layout.fragment_profile, container, false);
     }
 
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState){
-        super.onViewCreated(view, savedInstanceState);
-        ((TextView)view.findViewById(R.id.textView)).setText(getArguments().getString(TITLE));
-    }
-}*/
+}
--- app/src/main/java/com/example/ruhealthytwo/ProgramActivity.java
+++ app/src/main/java/com/example/ruhealthytwo/ProgramActivity.java
@@ -1,21 +1,10 @@
 package com.example.ruhealthytwo;
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.view.Menu;
 import android.widget.TextView;
 
-import com.google.android.material.snackbar.Snackbar;
-import com.google.android.material.navigation.NavigationView;
-
 import androidx.annotation.NonNull;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager2.widget.ViewPager2;
 
@@ -23,32 +12,54 @@ import com.example.ruhealthytwo.databinding.ActivityProgramBinding;
 import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 
 public class ProgramActivity extends AppCompatActivity {
-
-    private AppBarConfiguration mAppBarConfiguration;
-    private ActivityProgramBinding binding;
-
-    private TextView emailHeader;
-
-    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_profile);
+        setContentView(R.layout.tab_layout_design);
+
         TabLayout tabLayout = findViewById(R.id.tabLayout);
         ViewPager2 viewPager2 = findViewById(R.id.pager);
 
-        Adapter adapter = new Adapter(getSupportFragmentManager(), getLifecycle());
+        Adapter adapter = new Adapter(this);
         viewPager2.setAdapter(adapter);
 
-        new TabLayoutMediator(tabLayout, viewPager2, new TabLayoutMediator.TabConfigurationStrategy() {
+      /* new TabLayoutMediator(tabLayout, viewPager2, new TabLayoutMediator.TabConfigurationStrategy() {
             @Override
             public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
-                tab.setText("Tab " + (position + 1));
+                if(position == 0)
+                    tab.setText("Profile");
+                else if(position == 1)
+                    tab.setText("Map");
+                else
+                    tab.setText("People");
             }
-        }).attach();
+        }).attach();*/
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager2.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+        viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                tabLayout.getTabAt(position).select();
+            }
+        });
     }
 }
